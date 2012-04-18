647c5966702d88c46f39ad70d26c2eab3834c02f
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-04-18 23:41:25 +0930

    Add TODOs

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ $ php pear-to-composer.php > composer.json
 $ curl -s http://getcomposer.org/installer | php
 $ php pear-to-composer.php > composer.json && php composer.phar validate 
 
-
+= TODO =
+ * Fully cover all package 2.0 elements
+ * Work out the PHP deps, pecl deps, etc
+ * Handle min, max version indicators
+ * Consider detecting if there are phpunit flavoured tests and rendering require-dev bits
+ * Consider if channel = pear.php.net, automagically stating the github and pear URIs
 
 
--- pear-to-composer.php
+++ pear-to-composer.php
@@ -1,5 +1,5 @@
 <?php
-$package_file = dirname(__FILE__) . '/Console_Getargs/package.xml';
+$package_file = dirname(__FILE__) . '/package.xml-test';
 
 
 // TODO: Consider a proper API, ie: PEAR_PackageFileManager2 for later.
@@ -9,7 +9,9 @@ $composer = array();
 
 $composer['name'] = (string)$document->name;
 $composer['description'] = (string)$document->description;
-
+$composer['version'] = (string)$document->version->release;
+$composer['time'] = (string)$document->date;
+$composer['license'] = (string)$document->license;
 
 // TODO: Array merge and only when !empty, also contributors
 foreach ($document->lead as $author) {
@@ -19,29 +21,12 @@ foreach ($document->lead as $author) {
     );
 }
 
-
 foreach ($document->developer as $author) {
     $composer['authors'][] = array(
         'name' => (string)$author->name,
         'email' => (string)$author->email
     );
 }
-/*
-{
-    "authors": [
-        {
-            "name": "Nils Adermann",
-            "email": "naderman@naderman.de",
-            "homepage": "http://www.naderman.de"
-        },
-        {
-            "name": "Jordi Boggiano",
-            "email": "j.boggiano@seld.be",
-            "homepage": "http://seld.be"
-        }
-    ]
-}
-*/
 
 foreach ($document->dependencies->required->php as $dep) {
     // Yeah, probably wrong.
@@ -51,25 +36,7 @@ foreach ($document->dependencies->optional->package as $dep) {
     $composer['suggest'][(string)$dep->channel . '/' . (string)$dep->name] = "Optionally uses " . $dep->min;
 }
 
-/*
-<dependencies>
-  <required>
-   <php>
-    <min>4.1.0</min>
-   </php>
-   <pearinstaller>
-    <min>1.4.0b1</min>
-   </pearinstaller>
-  </required>
-  <optional>
-   <package>
-    <name>PHPUnit</name>
-    <channel>pear.phpunit.de</channel>
-    <min>3.6.0</min>
-   </package>
-  </optional>
- </dependencies>
-*/
+
 //print json_encode($composer, JSON_PRETTY_PRINT)."\n";
 print json_encode($composer)."\n";
 
